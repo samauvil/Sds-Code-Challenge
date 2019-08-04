bbaeaf590bff7d44b203c2a7f0a3833053b492ab
Sam Auvil <samuel.auvil@kroger.com>
2019-08-04 17:25:25 -0400

    Initial commit

---
+++ src/test/java/Challenge/SDS/Sam/SamApplicationTests.java
@@ -0,0 +1,16 @@
+package Challenge.SDS.Sam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
